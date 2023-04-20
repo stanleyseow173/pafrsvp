548b455b04ebd4b47264edf9f4c1e553bb718b1a
stanleyseow173 <seowszehowestanley@gmail.com>
2023-04-20 09:23:57 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/pafrsvp/PafrsvpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.pafrsvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafrsvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafrsvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/pafrsvp/PafrsvpApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.pafrsvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafrsvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
